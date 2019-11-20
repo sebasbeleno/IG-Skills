94e018dcd3487f25c61284f36bb5053fc8080bce
Camilo Morales S <moorales.camilo@gmail.com>
2019-11-20 15:10:16 -0500

    refactorizando codigo

---
+++ lib/src/pages/refactor.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Refactor extends StatelessWidget {
+  
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Text("data"),
+    );
+      
+  }
+}
