857422b1e528172965f9b6df4ae92d7d4bfee2f3
Camilo Morales S <moorales.camilo@gmail.com>
2019-11-20 15:22:17 -0500

    Refactorizando master

--- lib/src/pages/splash_screen.dart
+++ lib/src/pages/splash_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-class Screen extends StatelessWidget {
+class SplashScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
