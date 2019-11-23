d7ddca410a59107a60d325185524fc846b5e933e
Sebastian Beleño <39771116+SebastianHerrera@users.noreply.github.com>
2019-11-22 19:38:26 -0500

    AddSKills screen añadido por via "Upload"
    Al hacer push, no se subía el archivo "add_screen.dart" en la carpeta Pages, se sube de manera manual por github para solucionar esto.

---
+++ lib/src/pages/add_skills.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:ig_skills/src/ig_app.dart';
+
+class AddSkills extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+        appBar: AppBar(backgroundColor: Color.fromARGB(255, 60, 152, 255)),
+        body: Center(
+          child: Text("Add skills :D"),
+        ));
+  }
+}
